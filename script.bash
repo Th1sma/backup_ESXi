4587f4268a7eca6cb708653ee1b75284275845d3
Th1sma_ <95030586+Th1sma@users.noreply.github.com>
2023-11-06 18:18:57 +0100

    Update script.bash

--- script.bash
+++ script.bash
@@ -11,7 +11,7 @@ echo
 # Menu pour choisir les répertoires
 echo "Veuillez entrer les chemins des répertoires :"
 read -p "Répertoire local pour sauvegarder les fichiers téléchargés : " LOCAL_DIRECTORY
-read -p "Répertoire sur le serveur SFTP : " SFTP_REMOTE_DIRECTORY
+read -p "Répertoire sur le serveur SFTP (fichiers à télécharger) : " SFTP_REMOTE_DIRECTORY
 echo
 
 # Obtenir la date actuelle au format YYYYMMDD
@@ -31,45 +31,8 @@ download_file() {
 EOF
 }
 
-# Fonction pour décompresser un fichier gzip
-decompress_gzip_file() {
-    GZIPPED_FILE="$1"
-    DECOMPRESSED_FILE="$2"
-    
-    echo "--> Décompression de $GZIPPED_FILE vers $DECOMPRESSED_FILE"
-    
-    # Utiliser gzip pour décompresser le fichier
-    gunzip -c "$GZIPPED_FILE" > "$DECOMPRESSED_FILE"
-}
-
-# Fonction pour décompresser un fichier zip
-decompress_zip_file() {
-    ZIP_FILE="$1"
-    DESTINATION_DIRECTORY="$2"
-    
-    echo "--> Décompression de $ZIP_FILE vers $DESTINATION_DIRECTORY"
-    
-    # Utiliser unzip pour décompresser le fichier zip
-    unzip "$ZIP_FILE" -d "$DESTINATION_DIRECTORY"
-}
-
 # -------------- Téléchargement des fichiers depuis le serveur FTP -------------- #
 # Télécharger la BDD depuis le serveur SFTP
-download_file "$SFTP_REMOTE_DIRECTORY/mnails.$DAYTIME.dump.sql.gz" "$LOCAL_DIRECTORY/mnails.$DAYTIME.dump.sql.gz"
+download_file "$SFTP_REMOTE_DIRECTORY" "$LOCAL_DIRECTORY"
 
-# Télécharger les fichiers htdocs depuis le serveur SFTP
-download_file "$SFTP_REMOTE_DIRECTORY/mnails.$DAYTIME.htdocs.zip" "$LOCAL_DIRECTORY/mnails.$DAYTIME.htdocs.zip"
-
-# Décompresser le fichier BDD
-decompress_gzip_file "$LOCAL_DIRECTORY/mnails.$DAYTIME.dump.sql.gz" "$LOCAL_DIRECTORY/mnails.$DAYTIME.dump.sql"
-
-# Supprimer le fichier BDD compressé
-rm "$LOCAL_DIRECTORY/mnails.$DAYTIME.dump.sql.gz"
-
-# Décompresser le dossier HTDOCS
-decompress_zip_file "$LOCAL_DIRECTORY/mnails.$DAYTIME.htdocs.zip" "$LOCAL_DIRECTORY"
-
-# Supprimer le fichier HTDOCS compressé
-rm "$LOCAL_DIRECTORY/mnails.$DAYTIME.htdocs.zip"
-
-echo "--> Les fichiers sont bien téléchargés et décompressés."
+echo "--> Les fichiers sont bien téléchargés."

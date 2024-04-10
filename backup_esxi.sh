d4c46809348098a983957dc7b8007ccfff407aa4
Th1sma_ <95030586+Th1sma@users.noreply.github.com>
2024-04-10 15:02:54 +0200

    Update and rename script.bash to backup_esxi.sh

---
+++ backup_esxi.sh
@@ -0,0 +1,10 @@
+
+# Synchronisation de la configuration firmware de l'hôte ESXi avec la mémoire non volatile (NVRAM)
+vim-cmd hostsvc/firmware/sync_config
+
+# Création d'une sauvegarde de la configuration de l'hôte ESXi
+vim-cmd hostsvc/firmware/backup_config
+
+# Recherche des fichiers avec l'extension .tgz dans le répertoire /scratch/downloads/
+# et les copie vers le répertoire /vmfs/volumes/datastore1/ESXi_backup/ en les renommant avec la date et l'heure actuelles
+find /scratch/downloads/ -name "*.tgz" -exec cp {} /vmfs/volumes/datastore1/ESXi_backup/ESXi_config_backup_$(date +'%Y%m%d_%H%M%S').tgz \;
